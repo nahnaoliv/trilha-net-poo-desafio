b909f49383848b7ac886d634c211eeba60b34adb
Mariana Oliveira <91402583+nahnaoliv@users.noreply.github.com>
2024-05-10 04:55:29 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
+using System.Reflection;
+using System;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine($"- Nokia Tijolão");
+
+// Criar uma instância de Nokia
+Nokia nokia = new Nokia("31975279430", "", "", 10);
+
+nokia.Ligar();
+// Instalar um aplicativo no Nokia
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine(" ");
+
+Console.WriteLine($"- Iphone");
+
+// Criar uma instância de iPhone
+Iphone iphone = new Iphone("31975279430", "", "", 10);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+// Instalar um aplicativo no iPhone
+iphone.InstalarAplicativo("Instagram");
+
